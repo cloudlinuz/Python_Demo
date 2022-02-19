5928e4586588d256c3dd5e52049605e6401a86ad
admin <admin>
2022-02-19 14:04:55 +0530

    life in weeks

--- lifeinweeks.py
+++ lifeinweeks.py
@@ -12,5 +12,5 @@ print(f"Your remaining age is : {remaining_years}")
 print(f"Your remaining days is : {remaining_days}")
 print(f"Your remaining weeks is : {remaining_weeks}")
 print(f"Your remaining months is : {remaining_months}")
-
+#
 print(f"You have {remaining_days} days, {remaining_weeks} weeks, and {remaining_months} months left")
